621ce571883a8905422171db9b4e71fb7a36d928
margarito <margaritobalatero@gmail.com>
2020-07-26 00:09:17 +0800

    ok

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-   // MONGO_URI: 'mongodb://junjiedb:junjiedb51@ds127490.mlab.com:27490/apidb'
-   MONGO_URI: 'mongodb+srv://junjieapi:junjieapi@cluster0.p63jc.mongodb.net/dbjunjie?retryWrites=true&w=majority'
+   MONGO_URI: 'mongodb://junjiedb:junjiedb51@ds127490.mlab.com:27490/apidb'
+   // MONGO_URI: 'mongodb+srv://junjieapi:junjieapi@cluster0.p63jc.mongodb.net/dbjunjie?retryWrites=true&w=majority'
 }
