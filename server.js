570f1da71e8f9f91fdef9fc5795a98c8b9e76b38
marge <margaritobalatero@gmail.com>
2020-10-31 02:02:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,16 @@ const mongoose = require('mongoose');
 const { MONGO_URI } = require('./config');
 const bodyParser = require('body-parser');
 const app = express();
+var cors = require('cors')
 
 // Routes
 const postRoutes = require('./routes/api/posts');
-
-// using CORS
-app.use(function(req, res, next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept,Authorization"
-		);
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
-	next();
-});
 
 // BodyParser Middleware
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors()) // Use this after the variable declaration
 // Connect to MongoDB
 
 mongoose.connect(MONGO_URI, {
@@ -29,7 +20,7 @@ mongoose.connect(MONGO_URI, {
     useUnifiedTopology: true,
     useFindAndModify: false
 })
-    .then(() => console.log('MongoDB connected!'))
+    .then(() => console.log('MongoDB Atlas connected!'))
     .catch(err => console.log(err));
 
 // User routes
@@ -38,6 +29,3 @@ app.use('/api/posts', postRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server run at port ${PORT}'));
-
-
-// mongodb+srv://junjieapi:junjieapi@cluster0.p63jc.mongodb.net/dbjunjieatlas?retryWrites=true&w=majority
